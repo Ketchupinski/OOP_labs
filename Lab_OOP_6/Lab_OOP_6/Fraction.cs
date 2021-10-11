cbce200468030405d9a19e8f92572d16f3405d6b
Arsen <arsenko525@gmail.com>
2021-10-11 10:20:11 +0300

    Add new lab

---
+++ Lab_OOP_6/Lab_OOP_6/Fraction.cs
@@ -0,0 +1,186 @@
+﻿/*
+Створити клас Fraction для роботи з дробовими числами. Число має бути
+представлене двома полями:
+• ціла частина – довге ціле із знаком,
+• дробова частина – без-знакове коротке ціле.
+Реалізувати методи – арифметичні операції:
+• віднімання,
+• операції порівняння.
+*/
+
+using System;
+
+
+namespace Lab_OOP_6
+{
+    public class Fraction
+    { 
+        private long x; // numerator
+        private ushort y; // dominator
+
+        public Fraction()
+        {
+            x = 1;
+            y = 1;
+        }
+
+        public Fraction(long x, ushort y)
+        {
+            if (y == 0)
+            {
+                Console.WriteLine("y can't be 0! Changed to 1");
+                y = 1;
+            }
+            this.x = x;
+            this.y = y;
+        }
+
+        public Fraction(Fraction f)
+        {
+            f.x = this.x;
+            f.y = this.y;
+        }
+
+        ~Fraction()
+        {
+            x = 0;
+            y = 0;
+        }
+        public long getX()
+        {
+            return x;
+        }
+
+        public ushort getY()
+        {
+            return y;
+        }
+
+        public void setX(long x)
+        {
+            this.x = x;
+        }
+
+        public void setY(ushort y)
+        {
+            this.y = y;
+        }
+
+        public void Read()
+        {
+            // x
+            Console.WriteLine("x: ");
+            long x = Convert.ToInt64(Console.ReadLine());
+            this.x = x;
+            
+            // y
+            ushort y;
+            do
+            {
+                Console.WriteLine("y: ");
+                y = Convert.ToUInt16(Console.ReadLine());
+            } while (y == 0);
+
+            this.y = y;
+        }
+        
+        public static Fraction
+            operator ++(Fraction f)
+        {
+            return new Fraction(f.x + f.y, f.y);
+        }
+
+        public static Fraction
+            operator --(Fraction f)
+        {
+            return new Fraction(f.x - f.y, f.y);
+        }
+
+        public static Fraction
+            operator -(Fraction a, Fraction b)
+        {
+            long num = (a.x * b.y) - (a.y * b.x);
+            int denom = a.y * b.y;
+            return new Fraction(num, Convert.ToUInt16(denom));
+        }
+
+        public static bool
+            operator >(Fraction a, Fraction b)
+        {
+            double n1 = a.x / a.y;
+            double n2 = b.x / b.y;
+            if (n1 > n2)
+            {
+                return true;
+            }
+            return false;
+            
+        }
+        
+        public static bool
+            operator <(Fraction a, Fraction b)
+        {
+            double n1 = a.x / a.y;
+            double n2 = b.x / b.y;
+            if (n1 < n2)
+            {
+                return true;
+            }
+            return false;
+            }
+        
+        public static bool
+            operator <=(Fraction a, Fraction b)
+        {
+            double n1 = a.x / a.y;
+            double n2 = b.x / b.y;
+            if (n1 <= n2)
+            {
+                return true;
+            }
+            return false;
+
+        }
+        
+        public static bool
+            operator >=(Fraction a, Fraction b)
+        {
+            double n1 = a.x / a.y;
+            double n2 = b.x / b.y;
+            if (n1 >= n2)
+            {
+                return true;
+            }
+            return false;
+            
+        }
+        
+        public static bool
+            operator ==(Fraction a, Fraction b)
+        {
+            double n1 = a.x / a.y;
+            double n2 = b.x / b.y;
+            if (n1 == n2)
+            {
+                return true;
+            }
+            return false;
+            
+        }
+        
+        public static bool
+            operator !=(Fraction a, Fraction b)
+        {
+            double n1 = a.x / a.y;
+            double n2 = b.x / b.y;
+            if (n1 != n2)
+            {
+                return true;
+            }
+            return false;
+            
+        }
+        
+        
+    }
+}
---
+++ Lab_OOP_6/Lab_OOP_6/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Lab_OOP_6
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Fraction f = new Fraction();
+            f.Read();
+            
+            
+            f++;
+            Console.WriteLine("f++:\n  x: {0} | y: {1}", f.getX(), f.getY());
+            ++f;
+            Console.WriteLine("++f:\n  x: {0} | y: {1}", f.getX(), f.getY());
+            f--;
+            Console.WriteLine("f--:\n  x: {0} | y: {1}", f.getX(), f.getY());
+            --f;
+            Console.WriteLine("--f:\n  x: {0} | y: {1}", f.getX(), f.getY());
+
+            Fraction frac = new Fraction();
+            frac.Read();
+
+            Fraction result = f - frac;
+            Console.WriteLine("'f - frac':\n x: {0} | y: {1}", result.getX(), result.getY());
+
+            bool answ;
+            
+            answ = f > frac;
+            Console.WriteLine("\n'f > frac': {0}", answ);
+            
+            answ = f < frac;
+            Console.WriteLine("\n'f < frac': {0}", answ);
+            
+            answ = f <= frac;
+            Console.WriteLine("\n'f <= frac': {0}", answ);
+            
+            answ = f >= frac;
+            Console.WriteLine("\n'f >= frac': {0}", answ);
+            
+            answ = f == frac;
+            Console.WriteLine("\n'f == frac': {0}", answ);
+            
+            answ = f != frac;
+            Console.WriteLine("\n'f != frac': {0}", answ);
+
+        }
+    }
+}
